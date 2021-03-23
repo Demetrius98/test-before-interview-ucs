4ad1c64569c523749ec03baf08c1b08a561d10d9
dmitry <dmitry.bushmanov@gmail.com>
2021-03-24 00:42:41 +0300

    init project

---
+++ src/main/java/com/example/testbeforeinterview/TestBeforeInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbeforeinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBeforeInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBeforeInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testbeforeinterview/TestBeforeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbeforeinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBeforeInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
